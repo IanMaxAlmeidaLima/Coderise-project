9dbf19d59824678ae3c628c5174aeeda59149071
IanMaxAlmeidaLima <iancna360@gmail.com>
2025-06-18 02:52:11 -0300

    feat: Adição da funcionalidade do botãao de opções

---
+++ src/js/home.js
@@ -0,0 +1,6 @@
+const opcao_btn = document.getElementById('hamburger_btn');
+const opcoes = document.getElementById('opcoes');
+
+opcao_btn.addEventListener("click", function() {
+    opcoes.style.display = "flex";
+})
