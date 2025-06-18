f1b4bd0e24cc69f935757b2c5bf2b5f5a6343355
IanMaxAlmeidaLima <iancna360@gmail.com>
2025-06-18 05:59:11 -0300

    feat: Adicionada as funcionalidades atribuídas à div aberta pelo botão de menu

--- src/js/home.js
+++ src/js/home.js
@@ -1,6 +1,29 @@
-const opcao_btn = document.getElementById('hamburger_btn');
-const opcoes = document.getElementById('opcoes');
 
-opcao_btn.addEventListener("click", function() {
-    opcoes.style.display = "flex";
-})
+
+const btnOpcoes = document.getElementById('hamburger_btn');
+const divOpcoes = document.getElementById('opcoes');
+
+btnOpcoes.addEventListener('click', () => {
+  if (divOpcoes.style.display === 'none' || divOpcoes.style.display === '') {
+    divOpcoes.style.display = 'flex';  // mostra
+  } else {
+    divOpcoes.style.display = 'none';   // esconde
+  }
+});
+
+const acess = document.getElementById('acess');
+const vlibrasWidget = document.querySelector('div[vw]');
+
+acess.addEventListener('click', () => {
+  if (vlibrasWidget.style.display === 'none' || !vlibrasWidget.style.display) {
+    vlibrasWidget.style.display = 'block';
+  } else {
+    vlibrasWidget.style.display = 'none';
+  }
+});
+
+const suporte = document.getElementById('suporte');
+
+suporte.addEventListener('click', () => {
+    alert("Ligue para o número (71) 9 8751-4016 para tirar dúvidas")
+});
